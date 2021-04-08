c388199a48c425c23185407003a17afeadd53893
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2021-04-08 15:14:47 +1200

    Done for 8/4, test world

---
+++ Year13GameBraydenLeicester/Assets/Scripts/HandRotate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandRotate : MonoBehaviour
+{
+    public GameObject hand;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("r"))
+        {
+            hand.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
+        }
+    }
+}
