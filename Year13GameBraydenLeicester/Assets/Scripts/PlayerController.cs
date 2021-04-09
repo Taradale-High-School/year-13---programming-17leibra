9b29ffa0741bc558448a7db045d550d9538f4b39
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2021-04-09 13:07:38 +1200

    done for 9/4

---
+++ Year13GameBraydenLeicester/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //speed vars for the movements
+    private float moveSpeed = 10;
+    private float rotSpeed = 3;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime, 0, Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime); // moves player on wasd input (or arrows)
+        // for rotation on mouse posittion
+        Plane playerPlane = new Plane(Vector3.up, transform.position); //plane of the player
+        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition); // ray from camera to mouse pos
+        float hitDistance = 0.0f; //the distacnce that the two rays need to be from each other, 0
+        if (playerPlane.Raycast(cameraRay, out hitDistance))// to check if they are parallel
+        {
+            Vector3 targetPoint = cameraRay.GetPoint(hitDistance);//the point that is 0 from the camera ray
+            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position); // the rotation thats needed
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime); // rotates smoothly to the point
+
+
+        }
+    }
+}
