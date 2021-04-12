78149fa96ab66c7997460ff8619eed482aecad18
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2021-04-13 10:57:41 +1200

    done for 1st on 13/4

--- Year13GameBraydenLeicester/Assets/Scripts/PlayerController.cs
+++ Year13GameBraydenLeicester/Assets/Scripts/PlayerController.cs
@@ -6,38 +6,54 @@ public class PlayerController : MonoBehaviour
 {
     //speed vars for the movements
     private float moveSpeed = 5;
-    private float rotSpeed = 1;
-    public Camera mainCam;
-    //public GameObject cursor;
+    private float xRotation = 0f;
+    public float mouseSensitivity = 800f;
+    public float gravityAccleration = -9.81f;
+
+    private Vector3 velocity;
+
+    public float groundDistance = 0.3f; 
+
+    //Object refeferences 
+    public Transform cameraTransform;
+    public CharacterController controller;
+    public Transform groundCheck;
+    public LayerMask ground;
+
+   
+
 
 
     // Start is called before the first frame update
     void Start()
     {
-        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Input.GetAxis("Horizontal")*moveSpeed*Time.deltaTime, 0, Input.GetAxis("Vertical")*moveSpeed*Time.deltaTime); // moves player on wasd input (or arrows)
-                                                                                                                                          // for rotation on mouse posittion
-        Plane playerPlane = new Plane(Vector3.up, transform.position); //plane of the player
-        Ray cameraRay = Camera.main.ScreenPointToRay(Input.mousePosition); // ray from camera to mouse pos
-        float hitDistance = 0.0f; //the distacnce that the two rays need to be from each other, 0
-        if (playerPlane.Raycast(cameraRay, out hitDistance))// to check if they are parallel
-        {
-            Vector3 targetPoint = cameraRay.GetPoint(hitDistance);//the point that is 0 from the camera ray
-            Quaternion targetRotation = Quaternion.LookRotation(targetPoint - transform.position); // the rotation thats needed
-            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotSpeed * Time.deltaTime); // rotates smoothly to the point
-        }
+        //gets mouse position
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        //Vector3 mousePos = Input.mousePosition;
-        //Vector3 pos = mainCam.ScreenToWorldPoint(mousePos);
-        // cursor.transform.position = pos;
+        transform.Rotate(Vector3.up * mouseX); //rotates player
 
-        //Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        //transform.LookAt(mousePos*Time.deltaTime*rotSpeed);
-    
+        //sets and clamps camera rotaion, and rotates the camera
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        
+        //Wasd inputs and scaled to speeds
+        float xMove = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+        float zMove = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        
+        Vector3 totalMove = transform.right * xMove + transform.forward * zMove; // makes a vector3 to move through
+
+        controller.Move(totalMove); // moves through character controller component
+
+        velocity.y += gravityAccleration * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);  
     }
 }
