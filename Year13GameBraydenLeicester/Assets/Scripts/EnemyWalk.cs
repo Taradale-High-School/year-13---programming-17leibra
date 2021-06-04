1bedcc5ac0292452b976624451ce36088c5e415a
Brayden_Leicester <55036213+17leibra@users.noreply.github.com>
2021-06-04 13:30:39 +1200

    done for 4/6

---
+++ Year13GameBraydenLeicester/Assets/Scripts/EnemyWalk.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyWalk : MonoBehaviour
+{
+    //object References
+    public GameObject legLeft;
+    public GameObject legRight;
+    public GameObject ArmLeft;
+    public GameObject ArmRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+         
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
